9be06c00c6079c6bbb8773a4873268b6df7cb0ef
Joy-dpaula <joycedpaula136@gmail.com>
2024-08-06 15:51:56 -0300

    Add lib express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('Ola Mundo tudo bem com vocês');
+// const express = require('express');
+
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello World!",
+        success: "Sucesso ao executar API"
+    })
+});
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000')  
+});
